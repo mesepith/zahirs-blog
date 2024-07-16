eb27342961c388d726486aa0ad2c0e1c9bd9629f
mesepith <zahiralam29@gmail.com>
2024-07-16 19:44:46 +0530

    undo

--- wp-content/plugins/amp-copy-code-button/amp-copy-code-button.php
+++ wp-content/plugins/amp-copy-code-button/amp-copy-code-button.php
@@ -10,14 +10,11 @@ Author: Zahir Alam
 function amp_copy_code_button_scripts() {
     if ( function_exists( 'is_amp_endpoint' ) && is_amp_endpoint() ) {
         echo '<script async custom-element="amp-iframe" src="https://cdn.ampproject.org/v0/amp-iframe-0.1.js"></script>';
-        echo '<script async custom-element="amp-analytics" src="https://cdn.ampproject.org/v0/amp-analytics-0.1.js"></script>';
-        echo '<script async custom-element="amp-script" src="https://cdn.ampproject.org/v0/amp-script-0.1.js"></script>';
-
-        echo '<meta name="amp-script-src" content="sha384-w-Q5Hf2fy8mMQU5TTe6V283imDH_JYVjIY6ft2D8kczHtq6NSb1TRTRbtJRekZHh">';
     }
 }
 add_action( 'wp_head', 'amp_copy_code_button_scripts' );
 
+// Add copy button to code blocks
 function add_copy_button_to_code_blocks( $content ) {
     if ( function_exists( 'is_amp_endpoint' ) && is_amp_endpoint() ) {
         $pattern = '/<pre class="wp-block-code"><code>(.*?)<\/code><\/pre>/is';
@@ -35,49 +32,6 @@ function add_copy_button_to_code_blocks( $content ) {
                     </div>';
         };
         $content = preg_replace_callback( $pattern, $replacement, $content );
-
-        // Add AMP Analytics
-        $content .= '
-        <amp-analytics type="gtag" data-credentials="include">
-            <script type="application/json">
-            {
-              "vars" : {
-                "gtag_id": "G-27WDDW3TCL",
-                "config" : {
-                  "G-27WDDW3TCL": { "groups": "default" }
-                }
-              },
-              "triggers": {
-                "copyButtonClick": {
-                  "on": "amp-script-response",
-                  "request": "event",
-                  "vars": {
-                    "event_name": "copy_button_click",
-                    "event_category": "User Interaction",
-                    "event_label": "Copy Button"
-                  }
-                }
-              }
-            }
-            </script>
-        </amp-analytics>';
-
-        // Add AMP Script for handling copy button clicks
-        $content .= '
-        <amp-script script="copy-button-script" data-ampdevmode>
-            <script type="text/plain" target="amp-script" id="copy-button-script">
-                window.addEventListener("message", (event) => {
-                    if (event.data === "copy_button_clicked") {
-                        const copyEvent = new CustomEvent("amp-script-response", {
-                            detail: {
-                                name: "copy_button_click"
-                            }
-                        });
-                        window.dispatchEvent(copyEvent);
-                    }
-                });
-            </script>
-        </amp-script>';
     }
     return $content;
 }
