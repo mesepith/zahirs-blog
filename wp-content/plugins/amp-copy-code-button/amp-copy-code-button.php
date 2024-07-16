b16c94cbc57e3662f3cccc43e0bb15b9d09e49c2
mesepith <zahiralam29@gmail.com>
2024-07-16 17:10:08 +0530

    check 2

--- wp-content/plugins/amp-copy-code-button/amp-copy-code-button.php
+++ wp-content/plugins/amp-copy-code-button/amp-copy-code-button.php
@@ -11,6 +11,7 @@ function amp_copy_code_button_scripts() {
     if ( function_exists( 'is_amp_endpoint' ) && is_amp_endpoint() ) {
         echo '<script async custom-element="amp-iframe" src="https://cdn.ampproject.org/v0/amp-iframe-0.1.js"></script>';
         echo '<script async custom-element="amp-analytics" src="https://cdn.ampproject.org/v0/amp-analytics-0.1.js"></script>';
+        echo '<script async custom-element="amp-bind" src="https://cdn.ampproject.org/v0/amp-bind-0.1.js"></script>';
     }
 }
 add_action( 'wp_head', 'amp_copy_code_button_scripts' );
@@ -29,7 +30,6 @@ function add_copy_button_to_code_blocks( $content ) {
                         <amp-iframe sandbox="allow-scripts" width="94" height="72" frameborder="0" 
                                     src="' . $plugin_url . 'copier.html#' . rawurlencode($code) . '"
                                     layout="fixed">
-                            <button class="copy-button" data-label="' . htmlspecialchars($code, ENT_QUOTES, 'UTF-8') . '" placeholder disabled>Copy</button>
                         </amp-iframe>
                     </div>';
         };
