0a1372596bf0680946451f9075a2f9ba106ca0ec
mesepith <zahiralam29@gmail.com>
2024-07-16 17:04:10 +0530

    check

--- wp-content/plugins/amp-copy-code-button/amp-copy-code-button.php
+++ wp-content/plugins/amp-copy-code-button/amp-copy-code-button.php
@@ -28,7 +28,7 @@ function add_copy_button_to_code_blocks( $content ) {
                         <pre class="wp-block-code"><code id="code-' . $id . '">' . htmlspecialchars($code, ENT_QUOTES, 'UTF-8') . '</code></pre>
                         <amp-iframe sandbox="allow-scripts" width="94" height="72" frameborder="0" 
                                     src="' . $plugin_url . 'copier.html#' . rawurlencode($code) . '"
-                                    layout="fixed" class="i-amphtml-layout-fixed i-amphtml-layout-size-defined">
+                                    layout="fixed">
                             <button class="copy-button" data-label="' . htmlspecialchars($code, ENT_QUOTES, 'UTF-8') . '" placeholder disabled>Copy</button>
                         </amp-iframe>
                     </div>';
